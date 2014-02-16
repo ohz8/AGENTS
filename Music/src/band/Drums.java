754c28079f828f58664ab63759c1d0033f9bd327
Owen Isaacs <owen.isaacs88@gmail.com>
2014-02-16 19:51:30 +0000

    push

--- Music/src/band/Drums.java
+++ Music/src/band/Drums.java
@@ -1,12 +1,35 @@
 package band;
+import java.io.File;
+
 import org.jfugue.*;
 
 public class Drums {
 	public static void main(String[] args){
+		Tempo tempo = new Tempo(50);
+		
 		Player player = new Player();
 		Rhythm rhythm = new Rhythm();
-		rhythm.addSubstitution('O', "[ACOUSTIC_SNARE]q");
-		rhythm.setLayer(1, "'O' 'O' oo'O' oo'O' oo'O'");
-		player.play(rhythm);
+		rhythm.setLayer(1, "O..oO...O..oOO..");
+		rhythm.setLayer(2, "..*...*...*...*.");
+		rhythm.setLayer(3, "^^^^^^^^^^^^^^^^");
+		//rhythm.setLayer(4, "...............!");
+		rhythm.addSubstitution('O', "[BASS_DRUM]i");
+		rhythm.addSubstitution('o', "Rs [BASS_DRUM]s");
+		rhythm.addSubstitution('*', "[ACOUSTIC_SNARE]i");
+		rhythm.addSubstitution('^', "[PEDAL_HI_HAT]s Rs");
+		rhythm.addSubstitution('!', "[CRASH_CYMBAL_1]s Rs");
+		rhythm.addSubstitution('.', "Ri"); 
+		Pattern pattern = rhythm.getPattern();
+		File drumbeat = new File("drumbeat");
+		
+		try{
+			player.saveMidi(pattern, drumbeat);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		rhythm.getPattern();
+		
+		pattern.repeat(4);
 	}
 }
--- Music/src/band/Piano.java
+++ Music/src/band/Piano.java
@@ -1,9 +1,87 @@
 package band;
-import  org.jfugue.*;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.jfugue.Player;
 public class Piano {
+	
+	static ArrayList<Character> p_riff = new ArrayList<Character>();
+	
+	
+	public static double getKeyValue(String key)
+	{
+		char key2;
+		char key_tone;
+		double key_value = 0;
+		//check to see that one letter has been given
+		if (key.length()==1){
+			key2 = key.charAt(0);//cast string key as char
+			key_value = (double) key2;
+		}
+		else{
+			key2 = key.charAt(0);
+			key_tone = key.charAt(1);
+			if (key_tone == '#'){
+				key_value = (double) key2 + 0.5;
+			}
+			if (key_tone == 'b'){
+				key_value = (double) key2 - 0.5;	
+			}
+		}
+		return key_value;
+	}
+	
+	
+	public static ArrayList<Character> setRiff(String key){
+		
+		double kvalue = getKeyValue(key);
+			p_riff.add((char) kvalue);
+			p_riff.add((char) kvalue);
+			p_riff.add((char) (kvalue + 2.5) );
+			p_riff.add((char) (kvalue + 2.5) );
+			p_riff.add((char) kvalue);
+			p_riff.add((char) kvalue);
+			p_riff.add((char) (kvalue + 2.5) );
+			p_riff.add((char) (kvalue + 2.5) );
+		
+		return p_riff;
+	}
+	
 	public static void main (String[] args)
 	{
+		ArrayList<Character> new_riff= new ArrayList<Character>();
+		Player player = new Player(); //music player initialization
+		String play_riff = "";
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please select your key: ");
+		String new_key = input.nextLine();
+		
+		double k_value = getKeyValue(new_key);
+		
+		new_riff = setRiff(new_key);
+		for (int i = 0; i<new_riff.size(); i++){
+			play_riff = (play_riff + " " +  new_riff.get(i));
+	        System.out.println("The riff is " + play_riff);
+		}
+		
+		
+		
+		System.out.println("The key is " + new_key + " and the value is " + k_value);
+		player.play(play_riff);
+	}
+	
+	
+}
 			
+			
+			
+			
+			
+		
+		
+		/*
 			//Tempo rhythm = new Tempo(50);
 			//Pattern pattern = new Pattern("E6s D#6s | E6s D#6s E6s B5s D6s C6s | A5i.");
 			
@@ -38,3 +116,4 @@ public class Piano {
 	}
 	
 }
+*/
